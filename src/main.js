fb99b2d55cc98e418b7eb53d6f5824a09cc96f53
Sergioaero <116237665+Sergioaero1@users.noreply.github.com>
2024-10-16 21:59:54 -0300

    Upgrade
    bug resolver

--- src/main.js
+++ src/main.js
@@ -5,16 +5,20 @@ const cart = [];
 
 console.log("This is your Shopee cart");
 
-const item1 = await createItem("Manto do SPFC Masculino", 452.95, 20);
+const item1 = await createItem("Manto do SPFC Masculino", 452.95, 42);
 const item2 = await createItem("Manto do Flamengo Masculino", 472.45, 30);
-// const item3 = await createItem("Manto do SPFC Feminio", 352.25, 17);
-// const item4 = await createItem("Manto do Flamengo Feminino", 362.77, 13);
-// const item5 = await createItem("Manto do SPFC Infantil", 252.06, 25);
-// const item6 = await createItem("Manto do Flamengo Masculino", 228.95, 20);
+ const item3 = await createItem("Manto do SPFC Feminio", 352.25, 17);
+ const item4 = await createItem("Manto do Flamengo Feminino", 362.77, 13);
+ const item5 = await createItem("Manto do SPFC Infantil", 252.06, 25);
+ const item6 = await createItem("Manto do Flamengo Masculino", 228.95, 20);
 
-await cartService.addItem(myCart, item1);
-await cartService.addItem(myCart, item2);
+await cartService.addItem(cart, item1);
+await cartService.addItem(cart, item2);
+await cartService.addItem(cart, item3);
+await cartService.addItem(cart, item4);
+await cartService.addItem(cart, item5);
+await cartService.addItem(cart, item6);
 
 console.log("Shopee cart total is:");
-await cartService.calculateTotal(myCart);
+await cartService.calculateTotal(cart);
 
