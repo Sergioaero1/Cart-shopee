ee9a82cb46608444a3f9fefbeb4f6938a73c82f9
Sergioaero <116237665+Sergioaero1@users.noreply.github.com>
2024-10-18 01:19:13 -0300

    Upgrade
    Continue up...

--- src/main.js
+++ src/main.js
@@ -2,23 +2,30 @@ import * as cartService from "./service/cart.js"
 import createItem from "./service/item.js";
 
 const cart = [];
+const myWishList = [];
 
 console.log("This is your Shopee cart");
 
-const item1 = await createItem("Manto do SPFC Masculino", 452.95, 42);
-const item2 = await createItem("Manto do Flamengo Masculino", 472.45, 30);
- const item3 = await createItem("Manto do SPFC Feminio", 352.25, 17);
- const item4 = await createItem("Manto do Flamengo Feminino", 362.77, 13);
- const item5 = await createItem("Manto do SPFC Infantil", 252.06, 25);
- const item6 = await createItem("Manto do Flamengo Masculino", 228.95, 20);
+const item1 = await createItem("Manto do SPFC Masculino", 20.99, 1);
+const item2 = await createItem("Manto do Flamengo Masculino", 39.99, 3);
+const item3 = await createItem("Manto do SPFC Feminio", 352.25, 17);
+const item4 = await createItem("Manto do Flamengo Feminino", 362.77, 13);
+const item5 = await createItem("Manto do SPFC Infantil", 252.06, 25);
+const item6 = await createItem("Manto do Flamengo Masculino", 228.95, 20);
 
+// add itens ao carrinho
 await cartService.addItem(cart, item1);
 await cartService.addItem(cart, item2);
-await cartService.addItem(cart, item3);
-await cartService.addItem(cart, item4);
-await cartService.addItem(cart, item5);
-await cartService.addItem(cart, item6);
+// await cartService.addItem(cart, item3);
+// await cartService.addItem(cart, item4);
+// await cartService.addItem(cart, item5);
+// await cartService.addItem(cart, item6);
 
-console.log("Shopee cart total is:");
+await cartService.displayCart(cart);
+
+// deletar itens do carrinho
+await cartService.deleteItem(cart, item1);
+await cartService.deleteItem(cart, item2.name);
+
 await cartService.calculateTotal(cart);
 
--- src/service/cart.js
+++ src/service/cart.js
@@ -3,13 +3,28 @@ async function addItem(userCart, item) {
 }
 
 async function calculateTotal(userCart) {
+    console.log(`\n Shopee cart total is:`);
     const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
-    console.log(result);
+    console.log(`💲Total:${result}`);
 }
 
-async function deleteItem(userCart, name) {}
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name );
+
+    if(index !== -1 ) {
+        userCart.splice(index, 1);
+    }
+}
 
 async function removeItem(userCart, index) {}
 
-export {addItem, calculateTotal, deleteItem, removeItem};
+
+async function displayCart(userCart) {
+    console.log("SHOPEE CART LIST:");
+    userCart.forEach((item, index) => {
+        console.log(`\n ${index +1}. ${item.name} - R$ ${item.price}  |  ${item.quantity}  Unidades |  subtotal = ${item.subtotal()}`);
+    });
+}
+
+export {addItem, calculateTotal, deleteItem, removeItem, displayCart};
 
