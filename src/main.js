f9235f550f46be11114653335076befcb581387a
Sergioaero <116237665+Sergioaero1@users.noreply.github.com>
2024-10-20 22:42:01 -0300

    Cart Shopee
    Shopee Cart
    Modules are essential for organizing your code.

--- src/main.js
+++ src/main.js
@@ -2,30 +2,41 @@ import * as cartService from "./service/cart.js"
 import createItem from "./service/item.js";
 
 const cart = [];
-const myWishList = [];
+const myWishListItem = [];
 
-console.log("This is your Shopee cart");
-
-const item1 = await createItem("Manto do SPFC Masculino", 20.99, 1);
-const item2 = await createItem("Manto do Flamengo Masculino", 39.99, 3);
+// Criando itens para depois add, remover, ...
+const item1 = await createItem("Manto do SPFC Masculino", 402.99, 49);
+const item2 = await createItem("Manto do Flamengo Masculino", 452.99, 35);
 const item3 = await createItem("Manto do SPFC Feminio", 352.25, 17);
 const item4 = await createItem("Manto do Flamengo Feminino", 362.77, 13);
 const item5 = await createItem("Manto do SPFC Infantil", 252.06, 25);
-const item6 = await createItem("Manto do Flamengo Masculino", 228.95, 20);
+const item6 = await createItem("Manto do Flamengo Infantil", 228.95, 26);
 
 // add itens ao carrinho
 await cartService.addItem(cart, item1);
 await cartService.addItem(cart, item2);
-// await cartService.addItem(cart, item3);
-// await cartService.addItem(cart, item4);
-// await cartService.addItem(cart, item5);
-// await cartService.addItem(cart, item6);
+await cartService.addItem(cart, item3);
+await cartService.addItem(cart, item4);
+await cartService.addItem(cart, item5);
+await cartService.addItem(cart, item6);
+
+// remover/diminuir itens
+await cartService.removeItem(cart, item1);
+await cartService.removeItem(cart, item3);
+await cartService.removeItem(cart, item3);
+await cartService.removeItem(cart, item5);
+await cartService.removeItem(cart, item6);
+await cartService.removeItem(cart, item6);
+
+// add a lista de desejos
+//console.log(`\n Minha lista de desejos: \n`);
+//await cartService.myWishListItem(cart, item1);
 
 await cartService.displayCart(cart);
 
 // deletar itens do carrinho
-await cartService.deleteItem(cart, item1);
-await cartService.deleteItem(cart, item2.name);
+//await cartService.deleteItem(cart, item1.name);
+//await cartService.deleteItem(cart, item2.name);
 
 await cartService.calculateTotal(cart);
 
--- src/service/cart.js
+++ src/service/cart.js
@@ -2,6 +2,10 @@ async function addItem(userCart, item) {
     userCart.push(item);
 }
 
+async function myWishListItem(userCart, item) {
+    userCart.push(item);
+}
+
 async function calculateTotal(userCart) {
     console.log(`\n Shopee cart total is:`);
     const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
@@ -16,7 +20,27 @@ async function deleteItem(userCart, name) {
     }
 }
 
-async function removeItem(userCart, index) {}
+// remover/diminuir um item
+async function removeItem(userCart, item) {
+    const indexFound = userCart.findIndex((p) => p.name === item.name); // encontrar o indice do item
+    // caso não encontre o ittem...
+    if(indexFound == -1) {
+        console.log(`Item não encontrado`);
+        return; //se essa condição,  return para evitar execultar os if abaixo
+    }
+
+    // se item > 1, subtrair um item. 
+    if(userCart[indexFound].quantity > 1) {
+        userCart[indexFound].quantity -= 1
+        return; //se essa condição,  return para evitar execultar os if abaixo
+    }
+
+    // Se item = 1, deletar o item
+    if(userCart[indexFound.quantity == 1]) {
+        userCart.splice(indexFound, 1); // splice = cortar. Vai cortar 1 em indexFound
+        return;
+    }
+}
 
 
 async function displayCart(userCart) {
@@ -26,5 +50,5 @@ async function displayCart(userCart) {
     });
 }
 
-export {addItem, calculateTotal, deleteItem, removeItem, displayCart};
+export {addItem, calculateTotal, deleteItem, removeItem, displayCart, myWishListItem};
 
